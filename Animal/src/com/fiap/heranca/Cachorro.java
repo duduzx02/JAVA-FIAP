557c8dac2010e2f973046959d4039ff197bdcde5
duduzx02 <duduzx02@gmail.com>
2023-02-12 02:43:50 -0300

    Herança

---
+++ Animal/src/com/fiap/heranca/Cachorro.java
@@ -0,0 +1,16 @@
+package com.fiap.heranca;
+
+public class Cachorro extends Animal {
+	private String latido;
+
+	public String getLatido() {
+		return latido;
+	}
+
+	public void setLatido(String latido) {
+		this.latido = latido;
+	}
+	
+	
+
+}
